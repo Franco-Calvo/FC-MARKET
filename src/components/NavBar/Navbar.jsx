c829011e07ac777b84133568b30dd522f4bbbbf2
nicolasivantorres <nicolas.torres@est.fi.uncoma.edu.ar>
2023-04-27 09:55:11 -0300

    quite el h1

--- src/components/NavBar/Navbar.jsx
+++ src/components/NavBar/Navbar.jsx
@@ -23,7 +23,6 @@ export default function Navbar({ productRef }) {
   return (
     <div className="navbar">
       <nav>
-        <h1>Hola</h1>
         <Anchor className="navButton" onClick={scrollToUp}>
           Inicio
         </Anchor>
